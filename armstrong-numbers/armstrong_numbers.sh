5e3a24b1f16dfc3024b5143dc00f4b548103d16c
flavio <flavi2101@gmail.com>
2024-08-27 17:40:44 -0300

    armostrong-number
    iterate over each value and make operation to the power of the size and
    store this value in a variable call sum

--- armstrong-numbers/armstrong_numbers.sh
+++ armstrong-numbers/armstrong_numbers.sh
@@ -11,3 +11,14 @@ if [[ ${value:0:1} == "-" ]]; then
         ${is_negative:=1}
         shift 1
 fi
+size=${#1}
+for (( i=0; i < size ; i++)); do
+
+        unit=${value:i:1}
+        if (( unit >= 0)); then
+        sum=$((sum + unit**size))
+        else
+                echo "Parameter is not a number"
+                exit 1
+        fi
+done
