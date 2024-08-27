2017997740451eae3ebd2d07555fd728702a8d6c
flavio <flavi2101@gmail.com>
2024-08-27 19:10:29 -0300

    pangram
    associate array is set and after convert each letter to lowercase, check if indeed a letter, if so check if array already has this key as value if not, set this letter as a key, if so increment the value

---
+++ pangram/pangram.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+PRHASE_SIZE=${#1}
+
+if [[ $# -ne 1 ]]; then
+	echo false
+	exit 1
+fi      
+
+number=${#1}
+declare -A my_array
+
+for (( i=0; i < number; i++)); do
+	letter=${1:i:1}
+	letter=${letter@L}
+	
+	if [[ $letter =~ [a-z] ]]; then
+		if [[ -v my_array[$letter] ]];then
+			((my_array[$letter]++))
+		else
+			my_array[$letter]=1
+		fi
+	fi
+	
+done
