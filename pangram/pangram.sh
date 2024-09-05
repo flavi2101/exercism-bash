86f8b194fd1a5dcf2bb2d18b9d95039ad14e851e
flavio <flavi2101@gmail.com>
2024-09-04 21:29:43 -0300

    pangram
    refactor
    
    efficiency improvement

--- pangram/pangram.sh
+++ pangram/pangram.sh
@@ -1,35 +1,23 @@
 #!/usr/bin/env bash
 
-PRHASE_SIZE=${#1}
-
-if [[ $# -ne 1 ]]; then
-	echo false
-	exit 1
+if (( $# != 1 )); then
+    echo false
+    exit 0
 fi      
 
-number=${#1}
 declare -A my_array
-
-for (( i=0; i < number; i++)); do
-	letter=${1:i:1}
-	letter=${letter@L}
-	
-	if [[ $letter =~ [a-z] ]]; then
-		if [[ -v my_array[$letter] ]];then
-			((my_array[$letter]++))
-		else
-			my_array[$letter]=1
-		fi
-	fi
-	
+input=${1,,}
+input=${input//[^a-z]}
+for (( iter=${#input}-1; iter >= 0; --iter)); do
+    ((++my_array[${input:iter:1}]))
 done
 
 #for key in "${!my_array[@]}"; do
-#	echo "${key}:${my_array[$key]}"
+#    echo "${key}:${my_array[$key]}"
 #done
 
 if (( ${#my_array[*]} == 26)); then
-	echo true
+    echo true
 else
-        echo false
+    echo false
 fi
